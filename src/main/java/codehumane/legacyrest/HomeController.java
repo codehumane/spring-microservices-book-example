c99504a5745172c9a6532b24541fb7b054eb7366
codehumane <codehumane@gmail.com>
2017-10-05 23:48:37 +0900

    [Book, Spring Microservices] `/greeting` endpoint add with HATEOAS support

--- src/main/java/codehumane/legacyrest/HomeController.java
+++ src/main/java/codehumane/legacyrest/HomeController.java
@@ -4,15 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    public Greet sayHello() {
-        return new Greet("Hello World!");
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public HttpEntity<Greet> greeting(
+            @RequestParam(
+                    value = "name",
+                    required = false,
+                    defaultValue = "HATEOAS"
+            ) String name
+    ) {
+
+        Greet greet = new Greet("Hello " + name);
+        greet.add(linkTo(methodOn(HomeController.class).greeting(name)).withSelfRel());
+        return new ResponseEntity<>(greet, HttpStatus.OK);
     }
 }
 
@@ -20,7 +39,7 @@ public class HomeController {
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-class Greet {
+class Greet extends ResourceSupport {
 
     private String message;
 
