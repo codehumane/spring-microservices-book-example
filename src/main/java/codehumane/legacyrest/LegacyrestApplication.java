477f9241d8a2fc632af1ad41ce0e794891043b43
codehumane <codehumane@gmail.com>
2017-10-06 16:44:24 +0900

    Securing microservices with basic security

--- src/main/java/codehumane/legacyrest/LegacyrestApplication.java
+++ src/main/java/codehumane/legacyrest/LegacyrestApplication.java
@@ -2,7 +2,9 @@ package codehumane.legacyrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class LegacyrestApplication {
 
--- src/test/java/codehumane/legacyrest/LegacyrestApplicationTests.java
+++ src/test/java/codehumane/legacyrest/LegacyrestApplicationTests.java
@@ -3,6 +3,11 @@ package codehumane.legacyrest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +23,21 @@ public class LegacyrestApplicationTests {
 
     @Test
     public void testVanillaService() {
-        RestTemplate restTemplate = new RestTemplate();
+        final RestTemplate restTemplate = new RestTemplate();
         Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
         assertEquals("Hello World!", greet.getMessage());
+    }
+
+    @Test
+    public void testSecureService() {
+        String plainCreds = "guest:guest123";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Greet> response = restTemplate
+                .exchange("http://localhost:9090/greeting?name=World!", HttpMethod.GET, request, Greet.class);
+        assertEquals("Hello World!", response.getBody().getMessage());
     }
 }
