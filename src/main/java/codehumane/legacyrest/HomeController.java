d275f17aa39affe18bc38a854f2e37bc5c5e53f0
codehumane <codehumane@gmail.com>
2017-10-06 16:04:45 +0900

    [Book, Spring Microservices] Reading custom properties

--- src/main/java/codehumane/legacyrest/HomeController.java
+++ src/main/java/codehumane/legacyrest/HomeController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -16,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+@Slf4j
 @RestController
 public class HomeController {
+
+    private final Environment env;
+
+    public HomeController(Environment env) {
+        this.env = env;
+    }
 
     @RequestMapping("/greeting")
     @ResponseBody
@@ -29,9 +38,14 @@ public class HomeController {
             ) String name
     ) {
 
-        Greet greet = new Greet("Hello " + name);
+        Greet greet = newGreet(name);
         greet.add(linkTo(methodOn(HomeController.class).greeting(name)).withSelfRel());
         return new ResponseEntity<>(greet, HttpStatus.OK);
+    }
+
+    private Greet newGreet(String name) {
+        log.info("bootrest.customproperty "+ env.getProperty("bootrest.customproperty"));
+        return new Greet("Hello " + name);
     }
 }
 
